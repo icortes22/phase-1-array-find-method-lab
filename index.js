846fb473d5c075c003a9fc2d869e0dfc711aa178
Isamar <isamarcortes589@gmail.com>
2021-11-07 15:38:01 -0500

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-
+function superbowlWin(records){
+    let found = records.find(function(record){
+        return record.result === 'W'
+    })
+    console.log(found)
+    if(found){
+        return found.year
+    }
+}
+    // for(let record of records){
+    //     if(record.result === 'W'){  
+    //         return record.year     
+    //         console.log('record', record.result)}
+    // }
